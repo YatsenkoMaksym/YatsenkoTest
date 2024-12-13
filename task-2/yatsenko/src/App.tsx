2342350c242c9b0ac84239cf67e34177ee80bc5e
Maksym <maxyach6@gmail.com>
2024-12-13 21:50:35 +0200

    Updated index.css once again, to prevent borders from staying on divs (that are used for image border to also contain a button) Moved all images inot State, to update them with filter on Click Also added id, just to prevent some small errors Console likes to worry about indexes. Fixed month to show real month

--- task-2/yatsenko/src/App.tsx
+++ task-2/yatsenko/src/App.tsx
@@ -17,20 +17,36 @@ function App() {
   const imgs = document.getElementsByTagName("img");
   const [number, setNumber] = useState(imgs.length);
   const [imager, setImage] = useState<string | null>(null);
-  const array = [
-    one,
-    two,
-    three,
-    four,
-    five,
-    six,
-    seven,
-    eight,
-    nine,
-    ten,
-    eleven,
-    twelve,
-  ];
+  const [array,setArray]= useState(
+    [
+      one,
+      two,
+      three,
+      four,
+      five,
+      six,
+      seven,
+      eight,
+      nine,
+      ten,
+      eleven,
+      twelve,
+    ]
+  )
+  // const array = [
+  //   one,
+  //   two,
+  //   three,
+  //   four,
+  //   five,
+  //   six,
+  //   seven,
+  //   eight,
+  //   nine,
+  //   ten,
+  //   eleven,
+  //   twelve,
+  // ];
   return (
     <div className="relative">
       count :{" "}
@@ -43,40 +59,46 @@ function App() {
           get count
         </button>
       ) : (
-        number
+        <button onClick={() => {
+          setNumber(imgs.length);
+        }}>New number is - {number}. Click again to update</button> 
       )}
       <hr />
       time:{" "}
-      {`${date.getDate()}:${date.getMonth()}:${date.getFullYear()} ${date.getHours()}:${date.getUTCMinutes()}`}
+      {`${date.getDate()}:${date.getMonth()+1}:${date.getFullYear()} ${date.getHours()}:${date.getUTCMinutes()}`}
       {imager && (
         <div className="relative grid z-2 h-full place-items-center">
-          <div className="bg-slate-900 w-screen top-0 min-h-screen nine:h-[100vh] fixed overflow-auto opacity-90">
+          <div className="bg-slate-900 w-screen top-0 min-h-screen nine:h-[100vh] fixed overflow-auto opacity-90"></div>
+          <div className="bg-slate-600 rounded-xl p-16 z-[4] fixed top-5 nine:left-[20%] flex gap-5 max-w-[40rem]">
+            <img src={imager} className="w-4/5" />
+            <button
+              className="rounded-2xl bg-slate-500 p-10 place-self-center"
+              onClick={() => {
+                setImage(null);
+              }}
+            >
+              Close
+            </button>
           </div>
-            <div className="bg-slate-600 rounded-xl p-16 z-[4] fixed top-5 nine:left-[20%] flex gap-5 max-w-[40rem]">
-              <img src={imager} className="w-4/5" />
-              <button
-                className="rounded-2xl bg-slate-500 p-10 place-self-center"
-                onClick={() => {
-                  setImage(null);
-                }}
-              >
-                Close
-              </button>
-            </div>
-          
         </div>
       )}
       <div
         id="ruller"
         className="bg-slate-300 grid nine:grid-cols-4 nine:grid-rows-3 five:grid-cols-2 grid-cols-1 p-5 gap-4 w-4/5 place-self-center place-items-center"
       >
-        {array.map((item) => (
-          <img
-            src={item}
-            onClick={() => {
-              setImage(item);
-            }}
-          />
+        {array.map((item,id) => (
+          <div className="relative" key={id}>
+            <img
+            
+              src={item}
+              onClick={() => {
+                setImage(item);
+              }}
+            />
+            <button onClick={()=>{
+              setArray((prev)=>prev.filter((need)=>need!==item))
+            }} className="absolute top-5 left-5 bg-red-500 px-3 py-1 rounded-full">X</button>
+          </div>
         ))}
       </div>
     </div>
